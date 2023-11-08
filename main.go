341ad2bf9c8d88fd4bb04a80dafb87bcd98fea47
BrennaEpp <brennae@google.com>
2023-11-07 22:58:48 -0500

    feat: add option to enable pprof server

--- main.go
+++ main.go
@@ -18,9 +18,13 @@ import (
 	"golang.org/x/oauth2"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/api/option"
+
 	// Install google-c2p resolver, which is required for direct path.
 	_ "google.golang.org/grpc/balancer/rls"
 	_ "google.golang.org/grpc/xds/googledirectpath"
+
+	// Register the pprof endpoints under the web server root at /debug/pprof
+	_ "net/http/pprof"
 )
 
 var (
@@ -48,6 +52,11 @@ var (
 	// Here, worker id goes from <0 to NumberOfWorker>.
 	ObjectNamePrefix = "princer_100M_files/file_"
 	ObjectNameSuffix = ""
+
+	tracerName      = "princer-storage-benchmark"
+	enableTracing   = flag.Bool("enable-tracing", false, "Enable tracing with Cloud Trace export")
+	enablePprof     = flag.Bool("enable-pprof", false, "Enable pprof server")
+	traceSampleRate = flag.Float64("trace-sample-rate", 1.0, "Sampling rate for Cloud Trace")
 
 	eG errgroup.Group
 )
@@ -145,6 +154,22 @@ func ReadObject(ctx context.Context, workerId int, bucketHandle *storage.BucketH
 func main() {
 	flag.Parse()
 	ctx := context.Background()
+
+	if *enableTracing {
+		cleanup := enableTraceExport(ctx, *traceSampleRate)
+		defer cleanup()
+	}
+
+	// Start a pprof server.
+	// Example usage (run the following command while the script is running):
+	// go tool pprof http://localhost:8080/debug/pprof/profile?seconds=60
+	if *enablePprof {
+		go func() {
+			if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+				log.Fatalf("error starting http server for pprof: %v", err)
+			}
+		}()
+	}
 
 	var client *storage.Client
 	var err error
